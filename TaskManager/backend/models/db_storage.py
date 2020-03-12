5913309513c4312c09ecb63205f2f3410b961d8c
veromejia <veromejia.q@gmail.com>
2020-03-12 15:42:18 -0700

    task Manager

---
+++ TaskManager/backend/models/db_storage.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+"""DataBase Storage"""
+from backend.models.patient import Base, Patient
+from backend.models.prescription import Prescription
+from backend.models.task_x_prescription import Task_x_prescription
+from backend.models.task import Task
+from sqlalchemy import create_engine, update
+from sqlalchemy.orm import sessionmaker
+import json
+import os
+from pathlib import Path
+
+
+
+class DBStorage:
+    """Class DBStorage"""
+    __engine = None
+    __session = None
+    path = '{}/TaskManager'.format(str(Path.home()))
+    propfile ='{}/db_properties.json'.format( path )
+
+    def __init__(self):
+        """Constructor"""
+        with open(self.propfile) as file:
+            data = json.load(file)
+
+            for prop in data['props']:
+                user = prop['USER']
+                pwd = prop['PWD']
+                host = prop['HOST']
+                database = prop['DB']
+                """print('mysql+mysqldb://{}:{}@{}/{}'.format(user, pwd, host, database))
+                print ("")"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            user, pwd, host, database), pool_pre_ping=True)
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+
+    def get_session(self):
+        return self.__session
+
+    def all_patients(self):
+        patients = self.__session.query(Patient).order_by(Patient.id).all()
+        #self.__session.close()
+        return patients
+
+    def all_patients_by_id(self, id):
+        patients = self.__session.query(Patient).filter(Patient.id == id).one()
+        return patients
+
+    def all_prescription(self):
+        new_prescription = self.__session.query(
+        Prescription).order_by(Prescription.id).all()
+        self.__session.close()
+        return new_prescription      
+
+    def all_task(self):
+        new_task = self.__session.query(Task).order_by(Task.id).all()
+        return new_task
+        """for task in self.__session.query(
+                Task).order_by(Task.id).all():
+            print("{}: {}".format(task.id, task.status))"""
+        self.__session.close()
+
+    def task_by_command(self,command):
+        query = self.__session.query(Task).filter(Task.task_command == command).all()
+        return query
+        #task = self.__session.query(Task).filter(Task.task_command == command)
+        #for task in self.__session.query(Task.command).filter(command):
+    #def update_status_task(self,status):
+
+
+    def all_new_tasks(self):
+        query = self.__session.query(Task).filter(Task.status == 'NEW0').all()
+        return query
+
+    def all_task_x_prescription(self):
+        new_taskPrescription = self.__session.query(Task_x_prescription).all()
+        return new_taskPrescription
+        self.__session.close()
+
+    def update_task_status(self,taskid,status):
+        query = self.__session.query(Task).filter(Task.id == taskid).all()
+        query[0].status = status;
+        self.__session.commit()
+
+    def add_patient(self, patient):
+        self.__session.add(patient)
+        self.__session.commit()
+
+    #def add_patient(self, patient):
+    #    self.__session.add(patient)
+    #    self.__session.commit()
+
+    def add_prescription(self, prescription):
+        self.__session.add(prescription)
+        self.__session.commit()
+
+    def add_task(self, task):
+        self.__session.add(task)
+        self.__session.commit()
+
+    def add_task_x_prescription(self, task_x_prescription):
+        self.__session.add(task_x_prescription)
+        self.__session.commit()
+
+"""#new_patient= Patient(name="keiry", last_name="mejia", email='keiry@gmail.com', phone="415-444-444")
+#i = DBStorage().add_patient(new_patient)
+#new_prescription= Prescription(patient_id=1, medication="motrin", frequency="6hrs", start_dt="2020-02-27 10:10:10", end_dt="2020-03-07 10:10:10")
+#j= DBStorage().add_prescription(new_prescription)
+
+#x = DBStorage().all_patients()
+#y = DBStorage().all_prescription()
+#z = DBStorage().all_task_x_prescription()
+#zz = DBStorage().all_task()
+"""
---
+++ TaskManager/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+from crontab import CronTab
+from decimal import Decimal
+import getpass
+import os 
+
+crons = 0
+os_user = getpass.getuser()
+path = os.getcwd()
+cron = CronTab(user=os_user)
+cron_command ='python3 {}/taskmanager.py '.format(path) 
+job = cron.new(command=cron_command,comment="medr-taskmanager")
+job.minute.every(1)
+cron.write()
+
+print("The following cron's were created: ".format(crons))
+for item in cron:
+    print(item)   
